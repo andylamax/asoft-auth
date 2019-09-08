c22a74be0813418ee56c6f1447416b1ebb6c10a9
Anderson Lameck <andylamax@programmer.net>
2019-09-08 14:51:24 +0300

    bumped versions up

--- src/commonMain/kotlin/tz/co/asoft/auth/User.kt
+++ src/commonMain/kotlin/tz/co/asoft/auth/User.kt
@@ -57,7 +57,7 @@ open class User : Neo4JEntity {
         SignedOut
     }
 
-    fun getRef() = UserRef(uid, name, photoUrl)
+    val ref get() = UserRef(uid, name, photoUrl)
 
     companion object {
         val fake
