4d12e8c7d4066814528f173ff6af2cb7ffc9f4cb
Anderson Lameck <andylamax@programmer.net>
2019-10-07 14:58:36 +0300

    Upgraded to persist 8.0.0

--- src/commonMain/kotlin/tz/co/asoft/auth/dao/AuthLocalDao.kt
+++ src/commonMain/kotlin/tz/co/asoft/auth/dao/AuthLocalDao.kt
@@ -4,23 +4,12 @@ import kotlinx.serialization.json.Json
 import tz.co.asoft.auth.User
 import tz.co.asoft.persist.dao.Dao
 import tz.co.asoft.persist.storage.Storage
-import tz.co.asoft.platform.Ctx
-import tz.co.asoft.rx.lifecycle.LiveData
+import tz.co.asoft.platform.core.Ctx
 
 open class AuthLocalDao private constructor(ctx: Ctx, name: String) : Dao<User>(), IAuthLocalDao {
 
     private val db = Storage(ctx, name)
     private val serializer = User.serializer()
-
-    companion object {
-        private var instance: IAuthLocalDao? = null
-        /**
-         * It is advices to use auth package name for this one
-         */
-        fun getInstance(ctx: Ctx, name: String) = instance ?: AuthLocalDao(ctx, name).also {
-            instance = it
-        }
-    }
 
     override suspend fun create(user: User): User? {
         db.set("device_user", Json.stringify(serializer, user))
